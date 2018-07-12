b6da18e942459a9b74c0f2ada51b3df0398e33e4
Dmitry <dmitry@unipat.ru>
2018-07-12 14:40:14 +0300

    Small changes, removing limitations

--- get_movie_list.py
+++ get_movie_list.py
@@ -4,11 +4,11 @@ from bs4 import BeautifulSoup
 from psycopg2 import connect
 
 
-url_base = 'https://www.kinopoisk.ru'
-url = 'test.html'
-headers = {
+URL_BASE = 'https://www.kinopoisk.ru'
+HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'
 }
+DB_CONNECTION_STRING = ''
 
 
 def get_page(url, headers):
@@ -25,27 +25,27 @@ def db_write(sql_data):
             "like" = EXCLUDED."like",
             dislike = EXCLUDED.dislike
     """
-    with connect() as con:
+    with connect(DB_CONNECTION_STRING) as con:
         cur = con.cursor()
         cur.execute(sql_request, sql_data)
         con.commit()
 
 
 def main():
-    main_soup = BeautifulSoup(get_page('%s%s' % (url_base, '/top'), headers))
+    main_soup = BeautifulSoup(get_page('%s%s' % (URL_BASE, '/top'), HEADERS))
     movie_data = main_soup.find_all('tr', id=re_compile("^top250"))
     counter = 0
-    for item in movie_data[:1]:
+    for item in movie_data:
         counter += 1
         movie_link = item.find('a', {'class': 'all'}).get('href')
         print counter, movie_link
-        movie_url = '%s%s' % (url_base, movie_link)
-        movie_soup = BeautifulSoup(get_page(movie_url, headers))
+        movie_url = '%s%s' % (URL_BASE, movie_link)
+        movie_soup = BeautifulSoup(get_page(movie_url, HEADERS))
         movie_name = movie_soup.find('h1', {'itemprop': 'name'}).text
         positive = int(movie_soup.find('li', {'class': 'pos'}).find('b').text)
         negative = int(movie_soup.find('li', {'class': 'neg'}).find('b').text)
         db_write((movie_name, movie_url, positive, negative))
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
